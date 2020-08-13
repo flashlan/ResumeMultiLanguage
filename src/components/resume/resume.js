334cbd4abcef404ed54d020b0d4c2bf09683d2e4
flashlan <flash_lan@hotmail.com>
2020-08-13 17:08:21 -0300

    format

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,6 +1,4 @@
-//import i18n from 'i18next';
-//import { initReactI18next } from 'react-i18next';
-import React  from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -12,73 +10,73 @@ require("storm-react-diagrams/dist/style.min.css")
 library.add(fab);
 
 const Resume = () => {
-    const { t } = useTranslation(['translation', 'apresenta']);
-    return (
-      <React.Fragment>
-	<section id="resume">
-	  <div className="row education">
-	    <div className="three columns header-col">
-	    <h1><span>{t('apresenta:resume.education')}</span></h1>
-	    </div>
-	    <div className="nine columns main-col">
-	      <div className="row item">
-	        <div className="twelve columns">
-	        <h3>{t('apresenta:resume.colegtitle')}</h3>
-	        <p className="info">{t('apresenta:resume.colegsubtitle')} <span>•</span> <em className="date">{t('apresenta:resume.colegdate')}</em></p>
-	          <p>
-	          {t('apresenta:resume.colegtext')}
-	          </p>
-	        </div>
-	      </div> {/* item end */}
-	      <div className="row item">
-	        <div className="twelve columns">
-	        <h3>{t('apresenta:resume.faculdtitle')}</h3>
-	        <p className="info">{t('apresenta:resume.faculdsubtitle')} <span>•</span> <em className="date">{t('apresenta:resume.faculddate')}</em></p>
-	          <p>
-	          {t('apresenta:resume.faculdtext')}
-	          </p>
-	        </div>
-	      </div> {/* item end */}
-	    </div> {/* main-col end */}
-	  </div> {/* End Education */}
-	  {/* Work
+	const { t } = useTranslation(['translation', 'apresenta']);
+	return (
+		<React.Fragment>
+			<section id="resume">
+				<div className="row education">
+					<div className="three columns header-col">
+						<h1><span>{t('apresenta:resume.education')}</span></h1>
+					</div>
+					<div className="nine columns main-col">
+						<div className="row item">
+							<div className="twelve columns">
+								<h3>{t('apresenta:resume.colegtitle')}</h3>
+								<p className="info">{t('apresenta:resume.colegsubtitle')} <span>•</span> <em className="date">{t('apresenta:resume.colegdate')}</em></p>
+								<p>
+									{t('apresenta:resume.colegtext')}
+								</p>
+							</div>
+						</div> {/* item end */}
+						<div className="row item">
+							<div className="twelve columns">
+								<h3>{t('apresenta:resume.faculdtitle')}</h3>
+								<p className="info">{t('apresenta:resume.faculdsubtitle')} <span>•</span> <em className="date">{t('apresenta:resume.faculddate')}</em></p>
+								<p>
+									{t('apresenta:resume.faculdtext')}
+								</p>
+							</div>
+						</div> {/* item end */}
+					</div> {/* main-col end */}
+				</div> {/* End Education */}
+				{/* Work
 	----------------------------------------------- */}
-	  <div className="row work">
-	    <div className="three columns header-col">
-	    <h1><span>{t('apresenta:resume.work')}</span></h1>
-	    </div>
-	    <div className="nine columns main-col">
-	      <div className="row item">
-	        <div className="twelve columns">
-	        <h3>{t('apresenta:resume.work1title')}</h3>
-	        <p className="info">{t('apresenta:resume.work1subtitle')} <span>•</span> <em className="date">{t('apresenta:resume.work1date')}</em></p>
-	          <p>
-	          {t('apresenta:resume.work1text')}
-	          </p>
-	        </div>
-	      </div> {/* item end */}
-	      <div className="row item">
-	        <div className="twelve columns">
-	        <h3>{t('apresenta:resume.work2title')}</h3>
-	        <p className="info">{t('apresenta:resume.work2subtitle')} <span>•</span> <em className="date">{t('apresenta:resume.work2date')}</em></p>
-	          <p>
-	          {t('apresenta:resume.work2text')}
-	          </p>
-	        </div>
-	      </div> {/* item end */}
-	    </div> {/* main-col end */}
-	  </div> {/* End Work */}
-	  <div className="row skill">
-	    <div className="three columns header-col">
-	    <h1><span>{t('apresenta:resume.skills')}</span></h1>
-	    </div>
-	    <div className="nine columns main-col">
-	    <p>{t('apresenta:resume.skillstext')}<Link activeClass="activate" to="myroadmap"  spy={true} class="cursors-main pointer" >{t('apresenta:resume.toroadmap')} </Link>{t('apresenta:resume.skillstext2')}
-	      </p>
-	    </div> {/* main-col end */}
-	  </div> {/* End skills */}
-	</section>
-      </React.Fragment>
-    );
+				<div className="row work">
+					<div className="three columns header-col">
+						<h1><span>{t('apresenta:resume.work')}</span></h1>
+					</div>
+					<div className="nine columns main-col">
+						<div className="row item">
+							<div className="twelve columns">
+								<h3>{t('apresenta:resume.work1title')}</h3>
+								<p className="info">{t('apresenta:resume.work1subtitle')} <span>•</span> <em className="date">{t('apresenta:resume.work1date')}</em></p>
+								<p>
+									{t('apresenta:resume.work1text')}
+								</p>
+							</div>
+						</div> {/* item end */}
+						<div className="row item">
+							<div className="twelve columns">
+								<h3>{t('apresenta:resume.work2title')}</h3>
+								<p className="info">{t('apresenta:resume.work2subtitle')} <span>•</span> <em className="date">{t('apresenta:resume.work2date')}</em></p>
+								<p>
+									{t('apresenta:resume.work2text')}
+								</p>
+							</div>
+						</div> {/* item end */}
+					</div> {/* main-col end */}
+				</div> {/* End Work */}
+				<div className="row skill">
+					<div className="three columns header-col">
+						<h1><span>{t('apresenta:resume.skills')}</span></h1>
+					</div>
+					<div className="nine columns main-col">
+						<p>{t('apresenta:resume.skillstext')}<Link activeClass="activate" to="myroadmap" spy={true} class="cursors-main pointer" >{t('apresenta:resume.toroadmap')} </Link>{t('apresenta:resume.skillstext2')}
+						</p>
+					</div> {/* main-col end */}
+				</div> {/* End skills */}
+			</section>
+		</React.Fragment>
+	);
 }
 export default Resume;
